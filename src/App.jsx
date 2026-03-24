7fb5db77eaa157b3b1089042f7aa4e9ce5bd44f3
WaseurGIT <waseurrahman31@gmail.com>
2026-03-24 19:19:17 +0600

    snowfall added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Navbar";
+import Snowfall from "react-snowfall";
 
 function App() {
   return (
     <div>
+      <Snowfall color="#82C3D9"/>
       <div className="flex justify-center">
         <h1 className="text-blue-500 text-3xl font-bold underline">
           Basic Login And Registration Practice without Firebase Authentication
